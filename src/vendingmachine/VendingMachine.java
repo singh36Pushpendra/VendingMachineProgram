697a864d5f1c7691d6d0103677ff6e01d2ff884a
singh36Pushpendra <psthakur.1999@gmail.com>
2022-09-20 13:22:15 +0530

    Added: VendingMachine

---
+++ src/vendingmachine/VendingMachine.java
@@ -0,0 +1,38 @@
+package vendingmachine;
+
+import java.util.Scanner;
+
+public class VendingMachine {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter you money: ");
+        int money = scanner.nextInt();
+
+        int denominations[] = {1000, 500, 100, 50, 10, 5, 2, 1};
+
+        int note = 0;
+
+        while (money > 0) {
+
+            int count = 0;
+
+            System.out.println("Remaining money: " + money);
+
+            while (money >= denominations[note]) {
+
+                money = money - denominations[note];
+
+                count++;
+
+            }
+
+            System.out.println(denominations[note] + " X " + count);
+
+            note++;
+
+        }
+
+    }
+}
